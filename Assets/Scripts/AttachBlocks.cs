d56fca1ca45f957fd5a7e22c4448f9fe4d6fc8d5
bccummings <brandon.cl.cummings@gmail.com>
2021-11-20 18:39:36 -0500

    Add cannon model and player movement/firing

--- Assets/Scripts/AttachBlocks.cs
+++ Assets/Scripts/AttachBlocks.cs
@@ -26,10 +26,12 @@ public class AttachBlocks : MonoBehaviour
             {
 
                 // Calculate distance at nearest point
-                float distance = Vector3.Distance(
-                    iChild.GetComponent<Collider>().ClosestPoint(jChild.position),
-                    jChild.GetComponent<Collider>().ClosestPoint(iChild.position)
-                );
+                // float distance = Vector3.Distance(
+                //     iChild.GetComponent<Collider>().ClosestPoint(jChild.position),
+                //     jChild.GetComponent<Collider>().ClosestPoint(iChild.position)
+                // );
+
+                float distance = Vector3.Distance(iChild.localPosition, jChild.localPosition);
 
                 if (
                         (iChild != jChild) & // don't connect to self
@@ -37,6 +39,7 @@ public class AttachBlocks : MonoBehaviour
                         (iChild.tag == "Block") & (jChild.tag == "Block") & // only connect to parts labeled "block"
                         (distance <= distanceThreshold) // only connect if the parts are in close proximity
                         //TODO: check if there is a fixed joint going the opposite direction
+                        //TODO: Only want fixed joints in vertical directions, not horizontal
                     )
                 {
                     FixedJoint joint = iChild.gameObject.AddComponent<FixedJoint>();
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float shotCooldown = 3f;
+    [SerializeField] float shotVelocity = 500f;
+    float lastShotTime;
+    float NextFire;
+    [SerializeField] GameObject projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        // Input.GetAxis refers to Input Manager in Unity (Edit > Project Settings > Input Manager)
+        // Time.deltaTime gives period of each frame (1/Fs)
+        // moveSpeed is multiplier to speed up or slow down movement
+        float xValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        float yValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        bool fire = Input.GetButton("Fire1");
+
+
+        transform.Rotate(xValue, yValue, 0f, Space.World);
+
+        if (fire && Time.time > NextFire)
+        {
+            NextFire = Time.time + shotCooldown;
+            Fire();
+        }
+        
+    }
+    
+    void Fire()
+    {
+        GameObject clone = Instantiate(projectile, transform.position, transform.rotation);
+
+        Debug.Log("Fire! " + transform.rotation.x);
+        clone.GetComponent<Rigidbody>().AddRelativeForce(Vector3.up * shotVelocity * clone.GetComponent<Rigidbody>().mass);
+        //clone.GetComponent<Rigidbody>().AddForce(0f, shotVelocity * (1-transform.rotation.x), shotVelocity * transform.rotation.x);
+        //clone.GetComponent<Rigidbody>().AddForce(transform.up * shotVelocity);
+    }
+}
